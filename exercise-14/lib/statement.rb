562cd277d1043444858cce695f5f1548fbea5c04
rob patrick <robpatrick@alkalinezoo.com>
2015-07-22 16:21:48 +0100

    Now supports multiple calls.  Still need to generate the json for multiple calls.

--- exercise-14/lib/statement.rb
+++ exercise-14/lib/statement.rb
@@ -1,5 +1,5 @@
-require 'call_charges'
 require 'json'
+require 'money'
 
 class Statement
 
@@ -8,15 +8,16 @@ class Statement
   end
 
   def to_json
+
     {:generated => Date.today,
      :due => @due,
      :period => {:from => @from,
                  :to => @to },
-     :total => @due,
-     :callCharges => { :called => @call_charges.telephone_number,
-                       :date => @call_charges.call_date,
-                       :duration => @call_charges.call_duration,
-                       :cost => @call_charges.call_cost}
+     :total => @call_charges.total,
+     :callCharges => { :called => @call_charges.calls[0].telephone_number,
+                       :date => @call_charges.calls[0].call_date,
+                       :duration => @call_charges.calls[0].call_duration,
+                       :cost => @call_charges.calls[0].call_cost}
     }.to_json
   end
 
@@ -47,6 +48,47 @@ class Statement
     @call_charges = CallCharges.new(&block)
   end
 
+end
+
+class CallCharges
+
+  attr_reader :calls
+
+  def initialize(&block)
+    @calls = Array.new
+    instance_eval &block if block_given?
+  end
+
+  def call(telephone_number, &block)
+    @calls.push( Call.new(telephone_number, &block) ) if block_given?
+  end
+
+  def total
+    @calls.map{|call| call.call_cost || 0 }.inject(:+).round(2)
+  end
+
+end
+
+class Call
+
+  attr_reader :telephone_number, :call_date, :call_duration, :call_cost
+
+  def initialize(telephone_number, &block)
+    @telephone_number = telephone_number
+    instance_eval &block if block_given?
+  end
+
+  def date(date)
+    @call_date = date
+  end
+
+  def duration(duration)
+    @call_duration = duration
+  end
+
+  def cost(cost)
+    @call_cost = cost
+  end
 end
 
 class Fixnum
--- exercise-14/spec/statement_spec.rb
+++ exercise-14/spec/statement_spec.rb
@@ -17,23 +17,24 @@ describe Statement do
       call_charges do
         call '07716393769' do
           date Date.parse('2015-02-01')
-          duration "00:23:03"
+          duration '00:23:03'
           cost 1.13
         end
         call '07716393769' do
           date Date.parse('2015-02-01')
-          duration "00:23:03"
+          duration '00:23:03'
           cost 0.20
         end
       end
 
     end
     result = JSON.parse(statement.to_json)
+    p result
     expect(result['generated']).to eq(Date.today.to_s)
     expect(result['due']).to eq((Date.today + 30).to_s)
     expect(result['period']['from']).to eq('2015-01-26')
     expect(result['period']['to']).to eq('2015-02-25')
-    #expect(result['total']).to eq('1.23')
+    expect(result['total']).to eq(1.33)
   end
 
   describe '#to_json' do
@@ -42,6 +43,8 @@ describe Statement do
       statement = Statement.new do
         date Date.today
         call_charges do
+          call '999' do
+          end
         end
       end
       expect(statement.to_json).to include( '2015-07-22' )
@@ -51,6 +54,8 @@ describe Statement do
         date Date.today
         due(date + 30.days)
         call_charges do
+          call '999' do
+          end
         end
       end
       expect(statement.to_json).to include( '2015-08-21' )
@@ -59,35 +64,40 @@ describe Statement do
       statement = Statement.new do
         from Date.parse( '2015-01-26' )
         call_charges do
+          call '999' do
+          end
         end
       end
-      expect(statement.to_json).to include( "2015-01-26" )
+      expect(statement.to_json).to include( '2015-01-26' )
     end
     it 'statement should include a to date' do
       statement = Statement.new do
         to Date.parse( '2015-01-25' )
         call_charges do
+          call '999' do
+          end
         end
       end
-      expect(statement.to_json).to include( "2015-01-25" )
+      expect(statement.to_json).to include( '2015-01-25' )
     end
     it 'statement should include a call charges, call' do
       statement = Statement.new do
         call_charges do
-          call '07716393769'
+          call '07716393769' do
+          end
         end
       end
-      expect(statement.to_json).to include( "07716393769" )
+      expect(statement.to_json).to include( '07716393769' )
     end
     it 'statement should include a call charges, call, date' do
       statement = Statement.new do
         call_charges do
           call '07716393769' do
-            call Date.parse( '2015-02-01' )
+            date Date.parse( '2015-02-01' )
           end
         end
       end
-      expect(statement.to_json).to include( "2015-02-01" )
+      expect(statement.to_json).to include( '2015-02-01' )
     end
     it 'statement should include a call charges, call, duration' do
       statement = Statement.new do
@@ -97,7 +107,7 @@ describe Statement do
           end
         end
       end
-      expect(statement.to_json).to include( "00:23:03" )
+      expect(statement.to_json).to include( '00:23:03' )
     end
     it 'statement should include a call charges, call, cost' do
       statement = Statement.new do
@@ -107,7 +117,7 @@ describe Statement do
           end
         end
       end
-      expect(statement.to_json).to include( "1.13" )
+      expect(statement.to_json).to include( '1.13' )
     end
 
     it 'statement should include a date and call charges, call, date' do
@@ -119,8 +129,8 @@ describe Statement do
           end
         end
       end
-      expect(statement.to_json).to include( "2015-07-22" )
-      expect(statement.to_json).to include( "2015-02-01" )
+      expect(statement.to_json).to include( '2015-07-22' )
+      expect(statement.to_json).to include( '2015-02-01' )
     end
   end
 
